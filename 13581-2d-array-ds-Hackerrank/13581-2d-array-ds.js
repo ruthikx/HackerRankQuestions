5fe9cbad81c78ce1314075321d26d0ae0259a3c9
Ruthik M U <rruthik355@gmail.com>
2026-08-16 11:56:17 +0530

    Force committed solution - Time: O(1), Space: O(

---
+++ 13581-2d-array-ds-Hackerrank/13581-2d-array-ds.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hourglassSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function hourglassSum(arr) {
+    // Write your code here
+    let maxSum = -Infinity;
+    
+    for(let i=0; i <=3 ;i++){
+        for(let j=0; j <= 3 ; j++){
+            let sum = arr[i][j]+
+                arr[i][j+1]+
+                arr[i][j+2]+
+                arr[i+1][j+1]+
+                arr[i+2][j]+
+                arr[i+2][j+1]+
+                arr[i+2][j+2];   
+        maxSum = Math.max(maxSum,sum); 
+        }  
+    }
+    return maxSum
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = hourglassSum(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
