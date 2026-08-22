d60af794f0ccd8fb8cd6211ae84bd0572992ac71
Ruthik M U <rruthik355@gmail.com>
2026-08-22 12:15:59 +0530

    Force committed solution

---
+++ 20927-day-0-hello-world-Hackerrank/20927-day-0-hello-world.js
@@ -0,0 +1,23 @@
+
+
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable)
+    
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
