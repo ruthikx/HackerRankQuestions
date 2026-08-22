6c3e60914e3c72d2408a78587a1d5da96275fc3f
Ruthik M U <rruthik355@gmail.com>
2026-08-22 13:12:17 +0530

    Force committed solution

---
+++ 20996-day-7-regular-expressions-i-Hackerrank/20996-day-7-regular-expressions-i.js
@@ -0,0 +1,21 @@
+
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     */
+    
+    let re = /^([aeiou]).*\1$/;
+    
+    /*
+     * Do not remove the return statement
+     */
+    return re;
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
