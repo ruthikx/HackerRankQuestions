0b547700c636646ee6c9cb06d77108b005feadfa
Ruthik M U <rruthik355@gmail.com>
2026-08-23 15:01:27 +0530

    Force committed solution

---
+++ 21020-day-1-let-and-const-Hackerrank/21020-day-1-let-and-const.js
@@ -0,0 +1,16 @@
+
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    let r = Number(readLine());
+    
+    // Print the area of the circle:
+    console.log(PI*r**2)
+    // Print the perimeter of the circle:
+    console.log(2*PI*r)
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
